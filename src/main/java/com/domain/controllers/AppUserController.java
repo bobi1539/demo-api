8e38b82ea78c950fee993434686a1dfb141a61f5
root <root@Zero>
2021-07-23 20:37:18 +0700

    memperbaiki import requestbody di controller app user

--- src/main/java/com/domain/controllers/AppUserController.java
+++ src/main/java/com/domain/controllers/AppUserController.java
@@ -10,10 +10,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/users")
